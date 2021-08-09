5620781b082f15e86fd35d0833ed7c288dd2188e
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-08-09 08:14:31 -0500

    Simplificando formik en el formulario de Login

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Layout from '../components/Layout';
-import { useFormik } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const Login = () => {
-    // Formulario y validacion con Formik y Yup
-    const formik = useFormik({
+    // Formulario y validacion con Formik y Yup simplificado
+    const formikOptions = {
         initialValues: {
             email: '',
             password: ''
@@ -17,45 +17,43 @@ const Login = () => {
         onSubmit: valores => {
             console.log(valores);
         }
-    });
+    };
 
     return (
         <Layout>
-            <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
-                <h2 className="text-4l font-sans uppercase font-bold text-gray-800 text-center my-4">Iniciar sesion</h2>
+            <div className="mx-auto mb-32 md:w-4/5 xl:w-3/5">
+                <h2 className="my-4 font-sans font-bold text-center text-gray-800 uppercase text-4l">Iniciar sesion</h2>
                 <div className="flex justify-center mt-5">
                     <div className="w-full max-w-lg">
-                        <form className="bg-white-rounded shadow-md px-8 py-6 mb-4" onSubmit={ formik.handleSubmit }>
-                            <div className="mb-4">
-                                <label htmlFor="email" className="block text-block ml-0.5 text-xs font-bold mb-2">Email...</label>
-                                <input type="text" className="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" placeholder="Email del usuario..." value={ formik.values.email } onChange={ formik.handleChange } onBlur={ formik.handleBlur } />
-                                {
-                                    formik.touched.email && formik.errors.email
-                                        ? (
-                                            <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4">
+                        <Formik { ...formikOptions }>
+                            <Form>
+                                <label htmlFor="email" className="block text-block ml-0.5 text-xs font-bold mb-2">Email del usuario...</label>
+                                <Field name="email" type="email" className="w-full px-3 py-2 leading-tight text-gray-700 border shadow appearance-none focus:outline-none focus:shadow-outline" />
+                                <ErrorMessage name="email">
+                                    {
+                                        msg =>
+                                            <div className="p-4 my-2 text-red-700 bg-gray-200 border-l-4 border-red-500">
                                                 <p className="font-bold">Error</p>
-                                                <p>{ formik.errors.email }</p>
+                                                <p>{ msg }</p>
                                             </div>
-                                        )
-                                        : null
-                                }
-                            </div>
-                            <div className="mb-4">
+                                    }
+                                </ErrorMessage>
+
                                 <label htmlFor="password" className="block text-block ml-0.5 text-xs font-bold mb-2">Contraseña...</label>
-                                <input type="password" className="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" placeholder="Contraseña..." value={ formik.values.password } onChange={ formik.handleChange } onBlur={ formik.handleBlur } />
-                                {
-                                    formik.touched.password && formik.errors.password
-                                        ? (
-                                            <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4">
+                                <Field name="password" type="password" className="w-full px-3 py-2 leading-tight text-gray-700 border shadow appearance-none focus:outline-none focus:shadow-outline" />
+                                <ErrorMessage name="password">
+                                    {
+                                        msg =>
+                                            <div className="p-4 my-2 text-red-700 bg-gray-200 border-l-4 border-red-500">
                                                 <p className="font-bold">Error</p>
-                                                <p>{ formik.errors.password }</p>
+                                                <p>{ msg }</p>
                                             </div>
-                                        )
-                                        : null
-                                }
-                            </div>
-                            <button className="w-full text-sm text-white mt-8 bg-red-500 hover:bg-red-600 hover:text-gray-200 active:bg-red-500 font-bold uppercase block text-center px-4 py-3 rounded outline-none focus:outline-none  ease-linear transition-all duration-150" type="submit">Iniciar sesion</button>
-                        </form>
+                                    }
+                                </ErrorMessage>
+
+                                <button type="submit" className="block w-full px-4 py-3 mt-8 text-sm font-bold text-center text-white uppercase transition-all duration-150 ease-linear bg-red-500 rounded outline-none hover:bg-red-600 hover:text-gray-200 active:bg-red-500 focus:outline-none">Submit</button>
+                            </Form>
+                        </Formik>
                     </div>
                 </div>
             </div>
