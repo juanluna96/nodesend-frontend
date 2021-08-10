5f34dab426fa58293c848cf81ab45feae74433de
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-08-09 22:19:57 -0500

    Creando el Context de Autenticación

---
+++ context/auth/authState.js
@@ -0,0 +1,12 @@
+import React, { useReducer } from 'react'
+import authContext from './authContext';
+
+const AuthState = ({ children }) => {
+    return (
+        <authContext.Provider value={ {} }>
+            { children }
+        </authContext.Provider>
+    )
+}
+
+export default AuthState
