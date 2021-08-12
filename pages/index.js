45b8706b72c1b11058e979c686875004c99dfe6e
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-08-12 11:33:28 -0500

    Primeros pasos con la página principal

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import Layout from '../components/Layout';
 import authContext from '../context/auth/authContext';
+import Link from 'next/link';
 
 const Home = () => {
   // Extraer el usuario autenticado del storage
@@ -12,7 +13,20 @@ const Home = () => {
 
   return (
     <Layout>
-      <h1>Index2</h1>
+      <div className="mx-auto mb-32 md:w-4/5 xl:w-3/5">
+        <div className="p-5 py-10 bg-white rounded-lg lg:flex md:shadow-lg">
+          <p>Dropzone aquí</p>
+          <div className="mx-2 mt-16 mb-3 md:flex-1 lg:mt-0">
+            <h2 className="my-4 font-sans text-4xl font-bold text-gray-800">Compartir archivos de forma sencilla y rápida.</h2>
+            <p className="text-lg leading-loose">
+              <span className="italic font-bold text-red-500">ReactNode<span className="text-gray-800">Send</span></span> te permite compartir archivos con cifrado de extremo a extremo y un archivo que es eliminado después de ser descargado. Así que puedes mantener lo que compartes en privado y asegurarte de que tus cosas no permanezcan en línea para siempre.
+            </p>
+            <Link href="/login">
+              <a className="block my-3 text-base text-lg italic font-bold text-red-500 transition-all duration-150 ease-linear outline-none hover:text-red-700 active:text-red-800 focus:outline-none">Crear cuenta para mayores beneficios</a>
+            </Link>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
