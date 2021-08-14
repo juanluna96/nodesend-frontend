67e49935b37a6fc47cb6df629e8c0ce3b171b28c
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-08-13 19:17:46 -0500

    Mostrar el botón de Crear enlace una vez se sube el archivo

--- components/Dropzone.js
+++ components/Dropzone.js
@@ -1,10 +1,13 @@
 import React, { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import clienteAxios from '../config/axios';
+import { TiTimes } from 'react-icons/ti';
 import Loading from './Loading';
 
 const Dropzone = () => {
     const [loading, setLoading] = useState(false);
+    const [Error, setError] = useState({ file: false, delete: false });
+    const [archivos, setArchivos] = useState([]);
 
     const onDrop = useCallback(async (acceptedFiles) => {
         const [archivo] = acceptedFiles;
@@ -16,42 +19,83 @@ const Dropzone = () => {
         setLoading(true);
         try {
             const resultado = await clienteAxios.post('archivos', formData);
-            console.log(resultado.data);
-            setLoading(false);
+            archivo.nombre = resultado.data.archivo;
+
+            // Actualizando el state
+            setArchivos([archivo]);
+            setError({ ...Error, file: false })
         } catch (error) {
             console.log(error.response);
+            setError({ ...Error, file: true })
         }
+
+        setLoading(false);
     }, []);
 
-    // Extraer contenido de dropzone
-    const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({ onDrop });
+    const deleteFile = async (archivo) => {
+        try {
+            const nuevosArchivos = [...archivos]
+            nuevosArchivos.splice(nuevosArchivos.indexOf(archivo), 1)
+            setArchivos(nuevosArchivos);
+            await clienteAxios.delete(`archivos/${archivo.nombre}`, { data: { archivo: archivo.nombre } });
+        } catch (error) {
+            console.log(error.response);
+            setError({ ...Error, delete: archivo.nombre });
+        }
+    };
 
-    const archivos = acceptedFiles.map((archivo) => (
-        <li key={ archivo.lastModified } className="flex-1 p-3 mb-4 bg-white rounded shadow-lg">
-            <p className="text-xl font-bold">{ archivo.path }</p>
-            <p className="text-sm text-gray-500">{ (archivo.size / Math.pow(1024, 2)).toFixed() } MB</p>
+    const crearEnlace = () => {
+        console.log('Crear enlace');
+    }
+
+    // Extraer contenido de dropzone
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+    const listaArchivos = archivos.map((archivo, index) => (
+        <li key={ archivo.lastModified }>
+            <div className="float-right">
+                <button onClick={ () => deleteFile(archivo) } className="p-1 -mt-2 -mr-2 text-lg font-bold text-center text-white uppercase transition-all duration-150 ease-linear bg-red-500 rounded rounded-full outline-none hover:bg-red-600 hover:text-gray-200 active:bg-red-500 focus:outline-none"><TiTimes /></button>
+            </div>
+            <div className="flex-1 p-3 mb-4 bg-white rounded shadow-lg">
+                <p className="text-xl font-bold">{ archivo.path }</p>
+                <p className="text-sm text-gray-500">{ (archivo.size / Math.pow(1024, 2)).toFixed() } MB</p>
+            </div>
         </li>
     ))
 
     return (
         <div className="flex flex-col items-center justify-center mx-2 mt-16 mb-3 bg-gray-100 border-2 border-gray-400 border-dashed rounded-lg md:flex-1 lg:mt-0">
-            <ul className="mt-6">
-                { archivos }
-            </ul>
-            <div { ...getRootProps({ className: 'dropzone w-full py-32' }) }>
-                <input className="h-100" { ...getInputProps() } />
-                {
-                    loading ? (
-                        <Loading />
-                    ) :
-                        isDragActive ? <p className="text-2xl text-center text-gray-600">Suelta el archivo</p> : (
-                            <div className="text-center">
-                                <p className="text-2xl text-center text-gray-600">Seleccione un archivo y arrastralo aqui</p>
-                                <button className="px-4 py-3 my-10 mr-2 text-sm font-bold text-center text-white uppercase transition-all duration-150 ease-linear bg-red-500 rounded outline-none hover:bg-red-600 hover:text-gray-200 active:bg-red-500 focus:outline-none">Subir archivo</button>
-                            </div>
-                        )
-                }
-            </div>
+            {
+                listaArchivos.length > 0
+                    ? (
+                        <div className="w-full px-3 mt-10">
+                            <h4 className="mb-4 text-2xl font-bold text-center">Archivos</h4>
+                            <ul>
+                                { listaArchivos }
+                            </ul>
+                            { Error.delete && <p className="text-2xl text-center text-red-500">No se pudo eliminar el archivo { Error.delete }</p> }
+                            <button className="w-full px-4 py-3 my-5 text-sm font-bold text-center text-white uppercase transition-all duration-150 ease-linear bg-red-500 rounded outline-none hover:bg-red-600 hover:text-gray-200 active:bg-red-500 focus:outline-none" onClick={ crearEnlace }>Crear enlace</button>
+                        </div>
+                    )
+                    : (
+                        <div { ...getRootProps({ className: 'dropzone w-full py-32' }) }>
+                            <input className="h-100" { ...getInputProps() } />
+                            {
+                                loading ? <>
+                                    <Loading />
+                                </>
+                                    :
+                                    isDragActive ? <p className="text-2xl text-center text-gray-600">Suelta el archivo</p> : (
+                                        <div className="text-center">
+                                            { Error.file && <p className="text-xl italic text-center text-red-500">El archivo es muy grande</p> }
+                                            <p className="text-2xl text-center text-gray-600">Seleccione un archivo y arrastralo aqui</p>
+                                            <button className="px-4 py-3 my-10 mr-2 text-sm font-bold text-center text-white uppercase transition-all duration-150 ease-linear bg-red-500 rounded outline-none hover:bg-red-600 hover:text-gray-200 active:bg-red-500 focus:outline-none">Subir archivo</button>
+                                        </div>
+                                    )
+                            }
+                        </div>
+                    )
+            }
         </div>
     )
 }
